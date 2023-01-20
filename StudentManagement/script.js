c36734f6d5d5361d874461d16f4d46c58019c472
daravuth <chaukoldaravuth21@kit.edu.kh>
2023-01-20 22:58:56 +0700

    Student

--- StudentManagement/script.js
+++ StudentManagement/script.js
@@ -1,5 +1,41 @@
 var selectedRow = null
 
+const signUp = e => {
+    let email = document.getElementById('email').value,
+        pwd = document.getElementById('pwd').value;
+
+    let formData = JSON.parse(localStorage.getItem('formData')) || [];
+
+    let exist = formData.length && 
+        JSON.parse(localStorage.getItem('formData'));
+
+    if(!exist){
+        formData.push({ email, pwd });
+        localStorage.setItem('formData', JSON.stringify(formData));
+        document.querySelector('form').reset();
+        document.getElementById('email').focus();
+        alert("Account Created. Please Sign In using the link below.");
+    }
+    else{
+        alert("You have already signed up with this email");
+    }
+    e.preventDefault();
+}
+
+function signIn(e) {
+    let email = document.getElementById('email').value, pwd = document.getElementById('pwd').value;
+    let formData = JSON.parse(localStorage.getItem('formData')) || [];
+    let exist = formData.length && 
+    JSON.parse(localStorage.getItem('formData')).some(data => data.email.toLowerCase() == email && data.pwd.toLowerCase() == pwd);
+    if(!exist){
+        alert("Incorrect login credentials");
+    }
+    else{
+        location.href = "index.html";
+    }
+    e.preventDefault();
+}
+
 function onFormSubmit() {
         if(validate()) {
             var formData = readFormData();
